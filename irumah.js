cd34be5886f59fce9b8104842186dd119686fab0
tataputri <105484949+tataputri@users.noreply.github.com>
2023-04-14 22:53:16 +0700

    sabar

--- irumah.js
+++ irumah.js
@@ -1,13 +1,73 @@
-document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; bottom:0;z-index:9999">');
-document.write('<div style="text-align:center;display:block;max-width:729px;height:auto;overflow:hidden;margin:auto">');
+// adsterra popunder
+(function () {
+  let script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = "/pl19076969.highrevenuegate.com/d8/46/8e/d8468e4f9174769c29af3168f4e10604.js";
+  document.body.append(script);
+})();
 
-atOptions = {
-		'key' : '90b57d8bd827902ed9b631b8db146bfd',
-		'format' : 'iframe',
-		'height' : 90,
-		'width' : 728,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/90b57d8bd827902ed9b631b8db146bfd/invoke.js"></scr' + 'ipt>');
-document.write('</div>');
-document.write('</div>');
+// adsterra social bar
+// (function () {
+//   let script = document.createElement("script");
+//   script.type = "text/javascript";
+//   script.src =
+//     "//pl17383636.profitablecpmgate.com/18/a4/38/18a438b4c9c384a6f2d2033dccccef7b.js";
+//   document.body.append(script);
+// })();
+
+// floating ads
+(function () {
+  let div = document.createElement("div");
+  div.id = "fixedban";
+  div.style =
+    "width:100%;margin:auto;text-align:center;float:none;overflow:hidden;display:scroll;position:fixed;bottom:0;z-index:999;-webkit-transform:translateZ(0);";
+  let divA = document.createElement("div");
+  let a = document.createElement("a");
+  a.id = "close-fixedban";
+  a.setAttribute('onclick', 'document.getElementById("fixedban").style.display = "none";');
+  a.style = "cursor:pointer";
+  let img = document.createElement("img");
+  img.src = "https://yess-online.com/close.png";
+  img.style = "vertical-align: center;";
+  a.append(img);
+  divA.append(a);
+  let divScript = document.createElement("div");
+  divScript.style =
+    "text-align:center;display:block;max-width:728px;height:auto;overflow:hidden;margin:auto";
+
+  // INCLUDE ADS CODE HERE
+  // adsterra 300x250
+  let script = document.createElement("script");
+  script.type = "text/javascript";
+  script.innerHTML =
+    // "atOptions = { 'key': '65da7bd60a8a2aebe3b709920fdc42ef', 'format': 'iframe', 'height': 250, 'width': 300, 'params': {} };";
+    "atOptions = {'key' : '90b57d8bd827902ed9b631b8db146bfd','format' : 'iframe','height' : 90,'width' : 728,'params' : {} };";
+  let scripts = document.createElement("script");
+  scripts.type = "text/javascript";
+  scripts.src =
+    "//www.profitabledisplaynetwork.com/90b57d8bd827902ed9b631b8db146bfd/invoke.js";
+  // INCLUDE ADS CODE END
+
+  divScript.append(script);
+  divScript.append(scripts);
+
+  // Apppend to div
+  div.append(divA);
+  div.append(divScript);
+  document.body.append(div);
+})();
+
+
+// document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; bottom:0;z-index:9999">');
+// document.write('<div style="text-align:center;display:block;max-width:729px;height:auto;overflow:hidden;margin:auto">');
+
+// atOptions = {
+// 		'key' : '90b57d8bd827902ed9b631b8db146bfd',
+// 		'format' : 'iframe',
+// 		'height' : 90,
+// 		'width' : 728,
+// 		'params' : {}
+// 	};
+// 	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/90b57d8bd827902ed9b631b8db146bfd/invoke.js"></scr' + 'ipt>');
+// document.write('</div>');
+// document.write('</div>');
