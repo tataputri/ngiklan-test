63c58273409346903a76e59645896d2a473bbef8
tataputri <105484949+tataputri@users.noreply.github.com>
2023-04-14 22:38:12 +0700

    update terbaru

--- irumah.js
+++ irumah.js
@@ -1,18 +1,13 @@
 document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; bottom:0;z-index:9999">');
 document.write('<div style="text-align:center;display:block;max-width:729px;height:auto;overflow:hidden;margin:auto">');
 
-//Paste Iklan Disini
 atOptions = {
-		'key' : '7ab3fc02292382837aee16e237447158',
+		'key' : '90b57d8bd827902ed9b631b8db146bfd',
 		'format' : 'iframe',
 		'height' : 90,
 		'width' : 728,
 		'params' : {}
 	};
-	
-		document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/7ab3fc02292382837aee16e237447158/invoke.js"></scr' + 'ipt>');
-
-	// Batas Iklan
-
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/90b57d8bd827902ed9b631b8db146bfd/invoke.js"></scr' + 'ipt>');
 document.write('</div>');
 document.write('</div>');
